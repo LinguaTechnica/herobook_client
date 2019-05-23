b1d316e93eb2856b50fea5a69ab65dc43ad3a470
Kay Hudson <kay.hudson@galvanize.com>
2019-05-23 10:52:04 -0700

    Add hero model and specs.

---
+++ src/app/hero.ts
@@ -0,0 +1,13 @@
+export class Hero {
+  id: string;
+  name: string;
+  alignment: string;
+  gender: string;
+  eyeColor: string;
+  race: string;
+  hairColor: string;
+  skinColor: string;
+  height: string;
+  weight: string;
+  publisher: string;
+}
