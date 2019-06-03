113535f5628b15f524714ca80069a2e8499cf407
Kay Hudson <kay.hudson@galvanize.com>
2019-06-03 16:42:29 -0700

    Note and report known Angular testing issue
    'Possible issue: https://github.com/angular/angular/issues/25837'

--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -7,10 +7,9 @@ import { AppRoutingModule } from '../app-routing.module';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import {HeroListComponent} from '../hero-list/hero-list.component';
-import {AppComponent} from '../app.component';
-import {Hero} from '../hero';
-import {HeroDetailComponent} from '../hero-detail/hero-detail.component';
+import { HeroListComponent } from '../hero-list/hero-list.component';
+import { AppComponent } from '../app.component';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -35,21 +34,21 @@ describe('NavbarComponent', () => {
   beforeEach(() => {
     router = TestBed.get(Router);
     location = TestBed.get(Location);
-    router.initialNavigation();
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    // FIXME: Known Angular issues: https://github.com/angular/angular/issues/25837
+    router.initialNavigation();
   });
 
   it('should create', () => {
     const instance = new NavbarComponent();
-    expect(instance).toBeTruthy();
+    expect(instance).toBeTruthy('Possible issue: https://github.com/angular/angular/issues/25837');
   });
 
   it('should have nav brand logo', () => {
-    fixture = TestBed.createComponent(NavbarComponent);
     const el = fixture.nativeElement;
-    expect(el.querySelector('.brand-logo').innerText).toContain('Hero Book');
+    expect(el.querySelector('.brand-logo').innerText).toContain('Hero Book', 'Possible issue: https://github.com/angular/angular/issues/25837');
   });
 
   it('links should route to each component', fakeAsync(() => {
@@ -57,7 +56,7 @@ describe('NavbarComponent', () => {
     routes.map(route => {
       router.navigate([route]);
       tick();
-      expect(location.path()).toEqual(`/${route}`);
+      expect(location.path()).toEqual(`/${route}`, 'Possible issue: https://github.com/angular/angular/issues/25837');
     });
   }));
 });
